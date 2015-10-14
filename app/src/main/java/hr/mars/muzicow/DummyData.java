8ea058ed133ecf9d1ce94e30e02e931e3f6d6f1a
Hrvoje <hrvoje.orejas@gmail.com>
2015-10-14 18:48:52 +0200

    fix

---
+++ app/src/main/java/hr/mars/muzicow/DummyData.java
@@ -0,0 +1,27 @@
+package hr.mars.muzicow;
+
+/**
+ * Created by mars on 14/10/15.
+ */
+public class DummyData {
+    String songName;
+    String auhtorsName;
+    String youtubeUrl;
+    String genere;
+
+    public DummyData(String songName, String auhtorsName, String youtubeUrl, String genere) {
+        this.songName = songName;
+        this.auhtorsName = auhtorsName;
+        this.youtubeUrl = youtubeUrl;
+        this.genere = genere;
+    }
+// ovo je novi puush
+}
+/*
+    private void initializeData(){
+        dummyDataList = new ArrayList<>();
+        dummyDataList.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        dummyDataList.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        dummyDataList.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+*/
---
+++ app/src/main/java/hr/mars/muzicow/MainActivity.java
@@ -0,0 +1,67 @@
+package hr.mars.muzicow;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = new String[] {"a","b","c1"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
