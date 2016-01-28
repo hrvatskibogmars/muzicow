be5cf976668c2009d66394e1f0c206c5c9058be6
Ozren Krpan <ozkrpan@foi.hr>
2016-01-28 14:43:28 +0100

    Robotium UI test
    Praticipan
    -signing in
    Artis
    -signing in
    -creating event
    -editing artist info

---
+++ app/src/androidTest/java/hr/mars/muzicow/test/ArtistCreateEvent.java
@@ -0,0 +1,72 @@
+package hr.mars.muzicow.test;
+
+import hr.mars.muzicow.R;
+import hr.mars.muzicow.activities.MainActivity;
+import com.robotium.solo.*;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+
+public class ArtistCreateEvent extends ActivityInstrumentationTestCase2<MainActivity> {
+
+  	private Solo solo;
+
+  	public ArtistCreateEvent() {
+		super(MainActivity.class);
+  	}
+
+  	public void setUp() throws Exception {
+        super.setUp();
+		solo = new Solo(getInstrumentation(),getActivity());
+
+  	}
+  
+   	@Override
+   	public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+  	}
+  
+	public void testALogin()  throws Exception {
+        //Wait for activity: 'hr.mars.muzicow.activities.MainActivity'
+		solo.waitForActivity(hr.mars.muzicow.activities.MainActivity.class, 2000);
+
+        //Click on Participant
+		solo.clickOnView(solo.getView(hr.mars.muzicow.R.id.spinner));
+        //Wait for spinner to open
+		solo.waitForDialogToOpen(5000);
+        //Sleep for 1366 milliseconds
+		solo.sleep(1366);
+        //Click on Artist
+		solo.clickOnView(solo.getView(android.R.id.text1, 1));
+        //Sleep for 2242 milliseconds
+		solo.sleep(2242);
+        //Click on Log in with Twitter
+		solo.clickOnView(solo.getView(hr.mars.muzicow.R.id.twitter));
+		//wait for authorization
+        solo.waitForDialogToOpen(50000);
+        solo.enterText(2, "Testni event");
+        //wait for text to be entered
+        solo.waitForText("Testni event");
+        //enter tekst "Rock" in Genre
+        solo.enterText(3, "Rock");
+
+        //Click button create event
+        solo.clickOnButton("Create event");
+        //Wait 5000
+        solo.sleep(5000);
+        //Swipe RIGHT
+        solo.scrollToSide(solo.RIGHT);
+        //Wait
+        solo.sleep(1000);
+        //Swipe RIGHT
+        solo.scrollToSide(solo.RIGHT);
+        //enter new location
+        solo.enterText(1, "Knin, Hrvatska");
+        //wait
+        solo.sleep(5000);
+	}
+
+
+
+}
